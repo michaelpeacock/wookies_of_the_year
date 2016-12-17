bacff988db0250bedba1af85c2bdf50ef4d708fa
gledford <garry.ledford@gmail.com>
2016-12-16 23:21:41 -0500

    added buttons for ships

---
+++ Assets/Wookies/ClickShip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickShip : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Yeah1");
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Yeah2");
+    }
+}
