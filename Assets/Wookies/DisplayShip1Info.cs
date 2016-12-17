ed56629bb75c969efd943b4d67787a80a551f0a1
gledford <garry.ledford@gmail.com>
2016-12-17 01:00:38 -0500

    buttons now show data on the ship

---
+++ Assets/Wookies/DisplayShip1Info.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayShip1Info : MonoBehaviour {
+
+  private void DenFunc()
+  {
+        Debug.Log("displayPanel");
+  }
+
+}
