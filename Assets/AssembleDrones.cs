b147df856bb6265a97714ef467e29dd90e17e2b0
gledford <garry.ledford@gmail.com>
2016-12-17 05:58:13 -0500

    done with ships and distressed vessel

--- Assets/AssembleDrones.cs
+++ Assets/AssembleDrones.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AssembleDrones : MonoBehaviour {
-
-    public Button btn;
 
     // Use this for initialization
     void Start () {
---
+++ Assets/Sector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour {
+    public static bool isEnabled = false;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void enableSector() {
+        isEnabled = true;
+    }
+}
